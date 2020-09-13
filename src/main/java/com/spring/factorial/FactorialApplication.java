d3bf6abb71e29dde9909bc5d26c35f61fc44bb12
Caroline Martins <caroline.martins@ambar.tech>
2020-09-13 16:48:24 -0300

    [FEATURE] modify: excluded security class

--- src/main/java/com/spring/factorial/FactorialApplication.java
+++ src/main/java/com/spring/factorial/FactorialApplication.java
@@ -2,8 +2,9 @@ package com.spring.factorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class FactorialApplication {
 
 	public static void main(String[] args) {
