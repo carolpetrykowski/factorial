8938b977857223a1d57199eefd7a8f864868a8e0
Caroline Martins <caroline.martins@ambar.tech>
2020-09-15 14:52:53 -0300

    [FEATURE] modify: removed lines

--- src/main/java/com/spring/factorial/repository/FactorialRepository.java
+++ src/main/java/com/spring/factorial/repository/FactorialRepository.java
@@ -4,7 +4,4 @@ import com.spring.factorial.model.Calcs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FactorialRepository extends JpaRepository<Calcs, Integer>{
-
-
-
 }
